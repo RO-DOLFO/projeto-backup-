03183abf7c03a004c6d1805229ce441efd410948
RO-DOLFO <116039574+RO-DOLFO@users.noreply.github.com>
2024-04-29 13:05:45 -0300

    Add files via upload

---
+++ projeto.py
@@ -0,0 +1,32 @@
+import os
+from tkinter.filedialog import askdirectory
+import shutil
+import datetime 
+
+#pega o diretorio
+nome_pasta_selecionada = askdirectory()
+
+
+lista_arquivos = os.listdir(nome_pasta_selecionada)
+nome_pasta_backup = 'backup'
+
+nome_completo_pasta_backup = f'{nome_pasta_selecionada}/{nome_pasta_backup}'
+
+
+if not os.path.exists(nome_completo_pasta_backup):
+    os.mkdir(nome_completo_pasta_backup)
+
+data_atual = datetime.datetime.today().strftime('%d-%m-%y %H;%M')
+
+
+for arquivo in lista_arquivos:
+    nome_completo_arquivo = f'{nome_pasta_selecionada}/{arquivo}'
+    nome_final_arquivo = f'{nome_completo_pasta_backup}/{data_atual}/{arquivo}'
+
+    if not os.path.exists(f'{nome_completo_pasta_backup}/{data_atual}'):
+        os.mkdir(f'{nome_completo_pasta_backup}/{data_atual}')
+
+    if '.' in arquivo:
+        shutil.copy2(nome_completo_arquivo, nome_final_arquivo)
+    elif arquivo != 'backup':
+        shutil.copytree(nome_completo_arquivo, nome_final_arquivo)
